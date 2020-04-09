69cff02b51508c79e0789d3db3fbb5916ab79f93
taikipauel <63201219+taikipauel@users.noreply.github.com>
2020-04-09 23:23:53 +0900

    ステートの練習
    Section3-3を終了させた。
    リスト3-23, 3-24がよくわからんかった

--- src/App.js
+++ src/App.js
@@ -4,14 +4,79 @@ import './App.css';
 
 //表示をする要素を入れるコンポーネント
 class App extends Component {
+  data = [
+    "this is list sample",
+    "これはリストサンプルです",
+    "配列をリストに変換します"
+  ];
+
+  msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "20px 0",
+    padding: "5px,"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: this.data
+    };
+  }
 
   render() {
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="cyan" r="50" />
-      <Rect x="150" y="100" w="150" h="150" c="magenta" r="75" />
-      <Rect x="100" y="150" w="150" h="150" c="black" r="25" />
+      <h2 style={this.msgStyle}>show list.</h2>
+      <List title="サンプルリスト" data={this.data} />
     </div>;
+  }
+}
+
+class List extends Component {
+  number = 1;
+
+  title = {
+    fontSize: "20pt",
+    fontWeight: "bold",
+    color: "blue",
+  };
+
+  render() {
+    let data = this.props.data;
+    return(
+      <div>
+        <p style={this.title}>{this.props.title}</p>
+        <ul>
+          {data.map((item) => 
+            <Item number={this.number++} value={item} key={this.number} />
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Item extends Component {
+  li = {
+    listStyleType: "square",
+    fontSize: "16pt",
+    color: "#06",
+    margin: "0px",
+    padding: "0px",
+  }
+  num = {
+    fontWeight: "bold",
+    color: "red",
+  }
+
+  render() {
+    return (
+      <li style={this.li}>
+        <span style={this.num}>[{this.props.number}] </span>
+          {this.props.value}
+      </li>
+    );
   }
 }
 
--- src/Rect.js
+++ src/Rect.js
@@ -34,3 +34,66 @@ class Rect extends Component {
 }
 
 export default Rect;
+
+//リスト3-23
+// import React, { Component } from 'react';
+// import Rect from './Rect';
+// import './App.css';
+
+// //表示をする要素を入れるコンポーネント
+// class App extends Component {
+//   data = [];
+
+//   msgStyle = {
+//     fontSize: "20pt", 
+//     color: "#900",
+//     margin: "20px 0px",
+//     padding: "5px",
+//   }
+//   area = {
+//     width: "500px",
+//     height: "500px",
+//     border: "1px solid blue",
+//   }
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       list: this.data
+//     };
+//     this.doAction = this.doAction.bind(this);
+//   }
+
+//   doAction(e) {
+//     let x = e.pageX;
+//     let y = e.pageY;
+//     this.data.push({x:x, y:y}); ??????これは、どういうデータの形として配列に保存してるの??????
+//     this.setState({
+//       list: this.data 
+//     });
+//   }
+
+//   draw(d) {
+//     let s = {
+//       position: "absolute",
+//       left: (d.x - 25) + "px",
+//       top: (d.y - 25) + "px",
+//       width: "50px",
+//       height: "50px",
+//       backgroundColor: "#66f3",
+//     };
+//     return <div style={s}></div>;
+//   }
+
+//   render() {
+//     return <div>
+//       <h1>React</h1>
+//       <h2 style={this.msgStyle}>show rect.</h2>
+//       <div style={this.area} onClick={this.doAction}>
+//         {this.data.map((value) => this.draw(value))}
+//       </div>
+//     </div>;
+//   }
+// }
+
+// export default App;
