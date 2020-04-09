ad783a34e62626e9c1be95815e811da6ca832282
taikipauel <63201219+taikipauel@users.noreply.github.com>
2020-04-09 16:15:39 +0900

    コンポーネントの練習
    React超入門のSection3-2を終わらせた

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Rect from './Rect';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//表示をする要素を入れるコンポーネント
+class App extends Component {
+
+  render() {
+    return <div>
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" c="cyan" r="50" />
+      <Rect x="150" y="100" w="150" h="150" c="magenta" r="75" />
+      <Rect x="100" y="150" w="150" h="150" c="black" r="25" />
+    </div>;
+  }
 }
 
 export default App;
