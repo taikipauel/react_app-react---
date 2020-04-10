d78505da474460b48cfc1d06a2aaf15917257175
taikipauel <63201219+taikipauel@users.noreply.github.com>
2020-04-10 13:54:47 +0900

    コンポーネントの様々な機能を経験
    Section3-4を終了させた

--- src/App.js
+++ src/App.js
@@ -1,83 +1,53 @@
 import React, { Component } from 'react';
-import Rect from './Rect';
 import './App.css';
 
+let theme = {
+  light: {
+    backgroundColor: "#eef",
+    color: "#006",
+    padding: "10px",
+  },
+  dark: {
+    backgroundColor: "#006",
+    color: "#eef",
+    padding: "10px",
+  }
+};
+
+const ThemeContext = React.createContext(theme.dark);
 //表示をする要素を入れるコンポーネント
 class App extends Component {
-  data = [
-    "this is list sample",
-    "これはリストサンプルです",
-    "配列をリストに変換します"
-  ];
-
-  msgStyle = {
-    fontSize: "20pt",
-    color: "#900",
-    margin: "20px 0",
-    padding: "5px,"
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: this.data
-    };
-  }
+  static contextType = ThemeContext;
 
   render() {
-    return <div>
-      <h1>React</h1>
-      <h2 style={this.msgStyle}>show list.</h2>
-      <List title="サンプルリスト" data={this.data} />
-    </div>;
+    return <div style={this.context}>
+      <Title value="Content Page" />
+      <Message value="This is Content sample" />
+      <Message value="※これはテーマのサンプルです" />
+    </div>
   }
 }
 
-class List extends Component {
-  number = 1;
-
-  title = {
-    fontSize: "20pt",
-    fontWeight: "bold",
-    color: "blue",
-  };
+class Title extends Component {
+  static contextType = ThemeContext;
 
   render() {
-    let data = this.props.data;
-    return(
-      <div>
-        <p style={this.title}>{this.props.title}</p>
-        <ul>
-          {data.map((item) => 
-            <Item number={this.number++} value={item} key={this.number} />
-          )}
-        </ul>
-      </div>
+    return (
+      <h2 style={this.context}>{this.props.value}</h2>
     );
   }
 }
 
-class Item extends Component {
-  li = {
-    listStyleType: "square",
-    fontSize: "16pt",
-    color: "#06",
-    margin: "0px",
-    padding: "0px",
-  }
-  num = {
-    fontWeight: "bold",
-    color: "red",
-  }
+class Message extends Component {
+  static contextType = ThemeContext;
 
   render() {
     return (
-      <li style={this.li}>
-        <span style={this.num}>[{this.props.number}] </span>
-          {this.props.value}
-      </li>
+      <p style={this.context}>{this.props.value}</p>
     );
   }
 }
 
 export default App;
+
+
--- src/Rect.js
+++ src/Rect.js
@@ -35,7 +35,94 @@ class Rect extends Component {
 
 export default Rect;
 
-//リスト3-23
+
+//リスト3-24
+// import React, { Component } from 'react';
+// import Rect from './Rect';
+// import './App.css';
+
+// //表示をする要素を入れるコンポーネント
+// class App extends Component {
+//   data = [
+//     "this is list sample",
+//     "これはリストサンプルです",
+//     "配列をリストに変換します"
+//   ];
+
+//   msgStyle = {
+//     fontSize: "20pt",
+//     color: "#900",
+//     margin: "20px 0",
+//     padding: "5px,"
+//   }
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       list: this.data
+//     };
+//   }
+
+//   render() {
+//     return <div>
+//       <h1>React</h1>
+//       <h2 style={this.msgStyle}>show list.</h2>
+//       <List title="サンプルリスト" data={this.data} />
+//     </div>;
+//   }
+// }
+
+// class List extends Component {
+//   number = 1;
+
+//   title = {
+//     fontSize: "20pt",
+//     fontWeight: "bold",
+//     color: "blue",
+//   };
+
+//   render() {
+//     let data = this.props.data;
+//     return(
+//       <div>
+//         <p style={this.title}>{this.props.title}</p>
+//         <ul>
+//           {data.map((item) => 
+//             <Item number={this.number++} value={item} key={this.number} />
+//           )}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// class Item extends Component {
+//   li = {
+//     listStyleType: "square",
+//     fontSize: "16pt",
+//     color: "#06",
+//     margin: "0px",
+//     padding: "0px",
+//   }
+//   num = {
+//     fontWeight: "bold",
+//     color: "red",
+//   }
+
+//   render() {
+//     return (
+//       <li style={this.li}>
+//         <span style={this.num}>[{this.props.number}] </span>
+//           {this.props.value}
+//       </li>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+// //リスト3-23
 // import React, { Component } from 'react';
 // import Rect from './Rect';
 // import './App.css';
@@ -67,7 +154,7 @@ export default Rect;
 //   doAction(e) {
 //     let x = e.pageX;
 //     let y = e.pageY;
-//     this.data.push({x:x, y:y}); ??????これは、どういうデータの形として配列に保存してるの??????
+//     this.data.push({x:x, y:y});
 //     this.setState({
 //       list: this.data 
 //     });
@@ -90,7 +177,7 @@ export default Rect;
 //       <h1>React</h1>
 //       <h2 style={this.msgStyle}>show rect.</h2>
 //       <div style={this.area} onClick={this.doAction}>
-//         {this.data.map((value) => this.draw(value))}
+//         {this.state.list.map((value) => this.draw(value))}
 //       </div>
 //     </div>;
 //   }
